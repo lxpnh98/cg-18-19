da3bc54e8e7333dd783165665c1a2d3daeb932f8
Alexandre Pinho <lxpnh98@gmail.com>
2019-04-30 17:11:44 +0100

    Alterar modelos (ficheiros .3d).

---
+++ src/engine/loadTexture.cpp
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string>
+
+#include <IL/il.h>
+
+// Glut has to be included last to avoid errors
+// (for example, C2381 'exit': redefinition; '__declspec(noreturn)' or '[[noreturn]]' differs)
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glew.h>
+#include <GL/glut.h>
+#endif
+
+using namespace std;
+
+GLuint loadTexture(string file) {
+    GLuint texture;
+    unsigned int t, tw, th; 
+    unsigned char *texData;
+    ilGenImages(1, &t);
+    ilBindImage(t);
+    ilLoadImage((ILstring)file.c_str());
+    tw = ilGetInteger(IL_IMAGE_WIDTH);
+    th = ilGetInteger(IL_IMAGE_HEIGHT);
+    ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE);
+    texData = ilGetData();
+
+    glGenTextures(1, &texture);
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tw, th, 0, GL_RGBA, GL_UNSIGNED_BYTE, texData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    return texture;
+}
---
+++ src/engine/loadTexture.h
@@ -0,0 +1,6 @@
+#ifndef _TEXTURE
+#define _TEXTURE
+
+GLuint loadTexture(string file);
+
+#endif //_TEXTURE
